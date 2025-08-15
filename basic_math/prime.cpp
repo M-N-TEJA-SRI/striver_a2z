a4627f9868e515210651962c602cba6f62bdbd67
MANTHRI NRUSIMHA TEJA SRI <136962154+M-N-TEJA-SRI@users.noreply.github.com>
2025-08-15 19:17:43 +0530

    Create prime.cpp

---
+++ basic_math/prime.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    int i;
+    for(i=2;i*i<=n;i++){
+        if(n%i==0)
+        return false;
+    }
+    return true;
+}
+int main() {
+  int n;
+  cin>>n;
+cout<<isprime(n);
+    return 0;
+}
